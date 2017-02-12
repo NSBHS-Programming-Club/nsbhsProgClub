f31215daa257b8f7a9193a46b31484db95d09f26
Varun Sethu <vavu.seth3@gmail.com>
2017-02-12 10:22:02 +0000

    changes

--- join_us/php/signUp.php
+++ join_us/php/signUp.php
@@ -1,11 +1,12 @@
 <?php
-  if(isset($_POST['name']) && isset($_POST['email'])){
+$overide == true;
+  if(isset($_POST['name']) && isset($_POST['email']) && $overide == $false){
     $name = $_POST['name'];
     $email = $_POST['email'];
 
     $userFile = fopen("../../members.json", 'w+') or die("Can not open file");
       ini_set('allow_url_fopen', '1');
-      $jsonContetnt = file_get_contents("../../members.json") or die(phpinfo());
+      $jsonContetnt = file_get_contents("../../members.json") or die(header('Location: ../completesignup'));
         $jsonDB = json_decode($jsonContetnt, true);
           $members = &$jsonDB['members'];
             $members[$name] = array();
@@ -19,8 +20,8 @@
     file_put_contents("../../members.json", " ") or die("Could not erase file");
       file_put_contents("../../members.json", json_encode($jsonDB)) or die("Could not write to file");
         fclose($userFile);
-      header('Location: ../completesignup.php');
+      header('Location: ../completesignup');
     }else{
-      die(header('Location: ../index.php'));
+      die(header('Location: ../completesignup'));
     }
  ?>
